f37b83440539e3a311efde9b032b3af725d1b362
ZoulfaALBawab <sukkarsmiling89@hotmail.com>
2018-04-18 15:38:12 +0300

    ajax add

--- angular-client/index.js
+++ angular-client/index.js
@@ -1,1 +1,21 @@
-angular.module('meepo' , []);
+var app = angular.module('meepo' , []);
+
+app.controller('myctrl' , function ($scope,$http) {
+	//Ajax
+
+	$scope.done = function () {
+			var post = {
+		method :'POST',
+		url : '/user',
+		data :{
+			user : $scope.name
+		}
+	}
+	$http(post).then(function () {
+		console.log('success')
+	},function () {
+		console.log('error')
+	})
+	}
+
+})
