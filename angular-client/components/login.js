fe86470bdd8cedb06a63e75a150e0a7b1ba9cf17
QaysTrad <qaystrad@gamil.com>
2018-04-21 14:20:28 +0300

    edit stuff

--- angular-client/components/login.js
+++ angular-client/components/login.js
@@ -1,4 +1,38 @@
-angular.module('meepo')
-.component('login', {
+var app =angular.module('meepo')
+
+app.component('login', {
 	templateUrl :'/templates/login.html'
 });
+
+app.controller('login' , function ($scope,$http ) {
+	var get = function () {
+	var get={
+		method:"GET",
+		url : '/login'
+	}
+		$http(get).then(function (data) {
+			$scope.getter = data.data
+		},function () {
+			console.log('error')
+		})
+ }
+ var post = function (data) {
+	var post = {
+		method :'POST',
+		url : '/login',
+		data : data
+	}
+	$http(post).then(function () {
+		console.log('success')
+	},function () {
+		console.log('error')
+	})
+ 	
+ }
+	$scope.login = function () {
+		post({
+			username : $scope.nameLog ,
+			password : $scope.passwordLog 
+		})
+	}
+})
