12a31807129895485d48e42149d3921ad1f848c6
abdulhameed89 <abd.cs@hotmail.com>
2018-04-22 10:51:27 +0300

    add sing up

--- angular-client/components/signup.js
+++ angular-client/components/signup.js
@@ -1,4 +1,49 @@
-angular.module('meepo')
+var app = angular.module('meepo')
 .component('signup', {
 	templateUrl :'/templates/signup.html'
 });
+
+app.controller('signup' , function ($scope,$http ) {
+  var get = function () {
+	var get={
+		method:"GET",
+		url : '/user'
+	}
+		$http(get).then(function (data) {
+			console.log($scope.getter);
+			$scope.getter = data.data
+			console.log($scope.getter);
+		},function () {
+			console.log('error')
+		})
+ }
+ var post = function (data) {
+	var post = {
+		method :'POST',
+		url : '/user',
+		data : data
+	}
+	$http(post).then(function () {
+		console.log('success')
+	},function () {
+		console.log('error')
+	})
+ 	
+ }
+ 	get()
+
+	$scope.done = function () {
+	//Ajax
+	post({
+			username : $scope.name ,
+			password : $scope.password , 
+			email : $scope.email
+		})
+	get()
+	}
+	//abdulhameed transfare to login page
+	$scope.log = function () {
+
+	}
+	//=======
+})
