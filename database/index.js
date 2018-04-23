7958897dbf7435b65e2bb0af8ee474d855fc8aee
abdulhameed89 <abd.cs@hotmail.com>
2018-04-24 00:53:52 +0300

    fix style for the project

--- database/index.js
+++ database/index.js
@@ -3,24 +3,20 @@ let Schema =mongoose.Schema;
 mongoose.connect('mongodb://localhost:/PM-db' );
 // mongoose.connect('mongodb://admin:admin@ds249269.mlab.com:49269/pm-db' || 'mongodb://localhost/PM-db');
 var db = mongoose.connection;
-// new comment
 db.on('error' , function(){
 	console.log('mongoose not Connected !')
 })
 db.once('open' , function () {
 	console.log("mongoose conncted !")
 })
-
 var projectSchama = mongoose.Schema({
 	projectName : String , 
 	projectDisc : String
 })
-
 var userSchema = mongoose.Schema({
 	username :{type : String ,required :true, index :{unique:true} },
 	password :{type : String ,required :true}, 
 	email :{type : String ,required :true}, 
-	//projects : [{ type: Schema.Types.ObjectId, ref: 'Project' }]
 	projects:[projectSchama]
 	
 });
@@ -35,7 +31,6 @@ var taskSchema = mongoose.Schema({
 var User = mongoose.model("User" , userSchema);
 var Project = mongoose.model("Project" , projectSchama);
 var Task = mongoose.model('Task', taskSchema);
-
 var save = function (data , callback) {
 	var user = new User(data);
 	user.save(function (err , elem) {
@@ -43,7 +38,6 @@ var save = function (data , callback) {
 			callback(null ,elem)
 	})
 }
-
 var addTask = function(data, callback) {
 	var task = new Task(data);
 	task.save(function(err, data2) {
@@ -56,26 +50,14 @@ var addTask = function(data, callback) {
 }
 
 var addProject = function(data, callback) {
-	var project=new Project({projectName:data.projectName,projectDisc:data.projectDisc})
-console.log('dataaaaaaaaaa',data)
+var project=new Project({projectName:data.projectName,projectDisc:data.projectDisc});
 User.findById(data.project_id, function (err, user) {
-  if (err) return handleError(err);
-  user.projects.push(project);
-
-  //user.projects.push({_id:mongoose.Types.ObjectId(),projectName:data.projectName,projectDisc:data.projectDisc});
-  user.save();
-  project.save();
-});
-	// var project = new  User({projects:{projectName:data.projectName,projectDisc:data.projectDisc}});
-
-	// project.save(function(err,elem){
-	// 	if(err){
-	// 		callback(err,null)
-	// 	}
-	// 	callback(null,elem)
-	// });
+			  if (err) return handleError(err);
+			  user.projects.push(project);
+			  user.save();
+			  project.save();
+           });
 }
-
 module.exports.save = save;
 module.exports.User = User;
 module.exports.Project = Project;
