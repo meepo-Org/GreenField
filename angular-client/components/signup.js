3304e82f3ca43a3dd3f3bf11a207a193f0dd14fd
QaysTrad <qaystrad@gamil.com>
2018-04-26 14:21:45 +0300

    sign up disable

--- angular-client/components/signup.js
+++ angular-client/components/signup.js
@@ -41,5 +41,26 @@ app.controller('signup' , function ($scope,$http ) {
 
 	}
 	//=======
-})
+
+});
+
+angular.module('meepo').directive('disableBtn',
+	function() {
+		return {
+			restrict : 'A',
+			link : function(scope, element, attrs) {
+				var $el = $(element);
+				var submitBtn = $el.find('button[type="submit"]');
+				var fName = attrs.name;
+				
+				scope.$watch(fName + '.$valid', function(val) {
+					if (val) {
+						submitBtn.removeAttr('disabled');
+					} else {
+						submitBtn.attr('disabled', 'disabled');
+					}
+				});
+			}
+		};
+	})
 
