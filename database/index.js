e073e2c11c23b185a3df317d7e96cf74820dc1c1
abdulhameed89 <abd.cs@hotmail.com>
2018-04-26 15:26:37 +0300

    add task for all schemas

--- database/index.js
+++ database/index.js
@@ -10,9 +10,16 @@ db.on('error' , function(){
 db.once('open' , function () {
 	console.log("mongoose conncted !")
 })
+var taskSchema = mongoose.Schema({
+	description: String,
+	assignedTo: String,
+	complexity: Number,
+	status: String
+});
 var projectSchama = mongoose.Schema({
 	projectName : String , 
-	projectDisc : String
+	projectDisc : String,
+	tasks:[taskSchema]
 })
 var userSchema = mongoose.Schema({
 	username :{type : String ,required :true, index :{unique:true} },
@@ -22,13 +29,7 @@ var userSchema = mongoose.Schema({
 	
 });
 
-var taskSchema = mongoose.Schema({
-	description: String,
-	assignedTo: String,
-	complexity: Number,
-	status: String,
-	number: Number
-});
+
 
 var User = mongoose.model("User" , userSchema);
 var Project = mongoose.model("Project" , projectSchama);
@@ -48,14 +49,38 @@ var save = function (newUser , callback) {
 
 }
 var addTask = function(data, callback) {
-	var task = new Task(data);
-	task.save(function(err, data2) {
-		if(err)
-		{
-			callback(err, null);
-		}
-		callback(null, data2);
-	});
+	var task = new Task({description:data.description,assignedTo:data.assignedTo,complexity:data.complexity,status:data.status});
+	console.log("user idd",data.user_id);
+
+	User.findById(data.user_id, function (err, user) {
+			  if (err) return handleError(err);
+			  for(var i=0; i<user.projects.length ;i++){
+			  	console.log("projects[i] _id",user.projects[i]._id,typeof user.projects[i]._id);
+			  	console.log("data.project_id",data.project_id ,typeof data.project_id);
+
+			  	if(user.projects[i]._id.toString() === data.project_id){
+			  		console.log("I am in if statement")
+			  		Project.findById(data.project_id,function(err,project){
+			  			project.tasks.push(task);
+			  			project.save();
+			  		})
+			  		user.projects[i].tasks.push(task);
+
+			  		user.save();
+			  		task.save();
+			  	}
+			  }
+			  
+			  
+           });
+	/////////
+	// task.save(function(err, data2) {
+	// 	if(err)
+	// 	{
+	// 		callback(err, null);
+	// 	}
+	// 	callback(null, data2);
+	// });
 }
 
 var deleteTask = function(data, callback) {
